f04247e4455e513e9949b63a309187acdda5a98c
Dmitryi Zhilkin <spomidor8@gmail.com>
2023-09-17 20:14:19 +0300

    идите нахуй

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -10,10 +10,14 @@ const Button = ({
 }) => {
   return (
     <button
-      className={`mt -3 flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none
-      ${backgroundColor}?`${backgroundColor}${textColar}$[{borderColor}] 
-      \
-      bg-coral-red text-white border-coral-red border rounded-full`}
+      className={`mt -3  flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none
+      ${
+        backgroundColor
+          ? `${backgroundColor}${textColar}${borderColor}`
+          : "bg-coral-red text-white border-coral-red border "
+      }
+      rounded-full 
+     `}
     >
       {label}
       {iconURl && (
--- src/sections/CustonerReviews.jsx
+++ src/sections/CustonerReviews.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import ReviewCard from "../components/ReviewCard";
+import { reviews } from "../constants/constants";
 
 const CustonerReviews = () => {
   return (
-    <div>CustonerReviews</div>
-  )
-}
+    <section className="max-container ">
+      <h3 className=" font-palaquin text-center text-4xl font-bold ">
+        Waht Our
+        <span className="text-coral-red  "> Customers </span>
+        Say?
+      </h3>
+      <p className="info-text mt-4 m-auto max-w-lg text-center">
+        {" "}
+        Hear genuine storues from our satisfied customers about their
+        exceptional expirences with us
+      </p>
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14 ">
+        {reviews.map((reviews) => (
+          <ReviewCard
+            key={reviews.customerName}
+            customerName={reviews.customerName}
+            imgURL={reviews.imgURL}
+            rating={reviews.rating}
+            feedback={reviews.feedback}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default CustonerReviews
+export default CustonerReviews;
